713a4e2fb18eebb246e823e73ae77de9921ecbf9
Lélyan Ravida <lelyanravida@gmail.com>
2025-12-11 14:07:44 +0100

    feat: ajout d'un html avec node pour test

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import path from 'path';
+
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
 //app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'src/public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`)
