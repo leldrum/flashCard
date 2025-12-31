ecd7a5b443660c1c39a9ccc7339a2bd920fe7c93
Lélyan <lelyanravida@gmail.com>
2025-12-31 01:53:43 +0100

    feat: implementation du logger

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ import authRoutes from './router/authRouter.js'
 import path from 'path';
 import collectionRoutes from './router/collectionRouter.js'
 import cardRoutes from './router/cardRouter.js'
+import logger from './middleware/logger.js'
 
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+app.use(logger)
 
 app.use('/users', authRoutes)
 
