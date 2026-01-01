c70ebd6751cf9886fcb3810a9c7addbaa92fbb1c
Lélyan <lelyanravida@gmail.com>
2026-01-02 00:41:16 +0100

    feat: récup des révisions des cards ok

--- src/router/cardRouter.js
+++ src/router/cardRouter.js
@@ -1,7 +1,7 @@
 import {Router} from "express"
 
-import { validateBody } from "../middleware/validation.js"
-import { createCardSchema, updateCardSchema } from "../models/card.js"
+import { validateBody, validateParams } from "../middleware/validation.js"
+import { createCardSchema, updateCardSchema, idCardSchema } from "../models/card.js"
 import { createCard, getCardById, getCardsByCollection, getCardsToReview, deleteCard, updateCard } from "../controllers/cardController.js"
 import { authenticateToken } from "../middleware/authenticateToken.js"
 
@@ -9,10 +9,10 @@ const router = Router()
 router.use(authenticateToken)
 
 router.post('/', validateBody(createCardSchema), createCard)
-router.get('/collection/:id/review', getCardsToReview)
-router.get('/collection/:id', getCardsByCollection)
-router.get('/:id', getCardById)
+router.get('/collection/:id/review', validateParams(idCardSchema), getCardsToReview)
+router.get('/collection/:id', validateParams(idCardSchema), getCardsByCollection)
+router.get('/:id', validateParams(idCardSchema), getCardById)
 router.patch('/:id', validateBody(updateCardSchema), updateCard)
-router.delete('/:id', deleteCard)
+router.delete('/:id', validateParams(idCardSchema), deleteCard)
 
 export default router
